92bcfddcb0bc2eb7514e62a40bff1f8e628a509c
Caojunsheng <2296224958@qq.com>
2019-07-03 23:49:07 +0800

    修复android 6.0以上需要手动申请读写权限的bug

--- app/src/main/java/com/cjs/notebook/MainActivity.java
+++ app/src/main/java/com/cjs/notebook/MainActivity.java
@@ -1,14 +1,17 @@
 package com.cjs.notebook;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -40,11 +43,35 @@ public class MainActivity extends Activity implements OnScrollListener,
     private NotesDB DB;
     private SQLiteDatabase dbread;
 
+    //android6.0之后要动态获取权限
+    private void checkPermission() {
+        // Storage Permissions
+        final int REQUEST_EXTERNAL_STORAGE = 1;
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(MainActivity.this,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        checkPermission();
         tv_content = (TextView) findViewById(R.id.tv_content);
         listview = (ListView) findViewById(R.id.listview);
         dataList = new ArrayList<Map<String, Object>>();
